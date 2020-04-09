03a21d5aa650679a448089b76a4544a4b15f6bb1
yokokura <yokokura0413@gmail.com>
2020-04-10 02:20:38 +0900

    get request でデータを表示する実装

--- src/components/EventsIndex.js
+++ src/components/EventsIndex.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux'
+import _ from 'lodash';
 
 import { readEvents } from '../actions';
 
@@ -8,21 +9,43 @@ class EventsIndex extends Component {
     this.props.readEvents();
   }
 
-  render() {
-    const props = this.props;
-
+  renderEvents() {
     return (
-      <React.Fragment>
+      _.map(this.props.events, event => (
+        <tr key={event.id}>
+          <td>{event.id}</td>
+          <td>{event.title}</td>
+          <td>{event.body}</td>
+        </tr>
+      ))
+    );
+  }
 
-      </React.Fragment>
+  render() {
+    return (
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Title</th>
+            <th>Body</th>
+          </tr>
+        </thead>
+        <tbody>
+          {this.renderEvents()}
+        </tbody>
+      </table>
     );
   }
 }
 
-const mapStateToProps = state => ({ });
+const mapStateToProps = state => ({ events: state.events });
 
 const mapDispatchToProps = dispatch => ({
   readEvents: () => dispatch(readEvents()),
 })
 
-export default connect(mapStateToProps, mapDispatchToProps)(EventsIndex);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(EventsIndex);
