d9813122efbcf856fd8be7141ad7e138a956eff6
yokokura <yokokura0413@gmail.com>
2020-04-10 08:10:54 +0900

    Routerを使ってLinkを設定

--- src/index.js
+++ src/index.js
@@ -3,16 +3,23 @@ import ReactDOM from 'react-dom';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import './index.css';
 import reducer from './reducers';
 import EventsIndex from './components/EventsIndex';
+// import EventsNew from './components/EventsNew';
 
 const store = createStore(reducer, applyMiddleware(thunk));
 
 ReactDOM.render(
   <Provider store={store}>
-    <EventsIndex />
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={EventsIndex} />
+        {/* <Route exact path="/events/new" component={EventsNew} /> */}
+      </Switch>
+    </BrowserRouter>
   </Provider>,
   document.getElementById('root')
 );
