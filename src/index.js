fdb4ad091bc297e31936bde8b36ddbc70f3f4b0d
yokokura <yokokura0413@gmail.com>
2020-04-12 00:16:15 +0900

    devtool導入

--- src/index.js
+++ src/index.js
@@ -4,13 +4,16 @@ import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 import './index.css';
 import reducer from './reducers';
 import EventsIndex from './components/EventsIndex';
 import EventsNew from './components/EventsNew';
 
-const store = createStore(reducer, applyMiddleware(thunk));
+const enhancer = process.env.NODE_ENV === 'development' ?
+  composeWithDevTools(applyMiddleware(thunk)) : applyMiddleware(thunk)
+const store = createStore(reducer, enhancer)
 
 ReactDOM.render(
   <Provider store={store}>
